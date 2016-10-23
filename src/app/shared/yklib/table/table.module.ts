c2071dd5b94709909944b4f4cee1d808986ac0bb
杨恺 <njustyangkai@126.com>
2016-10-23 12:59:45 +0800

    table

--- src/app/shared/yklib/table/table.module.ts
+++ src/app/shared/yklib/table/table.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { TableComponent } from './table.component';
 import { CommonModule } from '@angular/common';
+import { CheckboxModule } from "../checkbox/checkbox.module";
 
 @NgModule({
   declarations:[
       TableComponent
   ],
   imports:[
-      CommonModule
+      CommonModule,
+      CheckboxModule
   ],
   exports:[
       TableComponent
