0c4c513fb2776698d89add5c7a81c1306b19318f
10195418 <yang.kai5@zte.com.cn>
2016-10-24 16:39:45 +0800

    sss

--- src/app/shared/yklib/checkbox/checkbox.component.ts
+++ src/app/shared/yklib/checkbox/checkbox.component.ts
@@ -1,55 +1,25 @@
 import {
     Component,
     Input,
-    Renderer,
-    ElementRef,
-    forwardRef
+    Output
 } from '@angular/core';
 
-import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
-
-const CONTROL_VALUE_ACCESSOR:any = {
-  provide: NG_VALUE_ACCESSOR,
-  useExisting: forwardRef(() => CheckboxComponent),
-  multi: true
-};
 
 @Component({
   selector: 'yk-checkbox',
   templateUrl: './checkbox.component.html',
-  styleUrls: ['./checkbox.component.css'],
-  providers: [CONTROL_VALUE_ACCESSOR]
+  styleUrls: ['./checkbox.component.css']
 })
 
-export class CheckboxComponent implements ControlValueAccessor {
-  setDisabledState(isDisabled:boolean):void {
-  }
+export class CheckboxComponent {
 
   @Input() disabled:boolean;
   @Input() text:string;
   @Input() checked:boolean;
-  @Input() value:any = null;
+  @Input() value:any;
+  @Output() 
 
-  constructor(private _renderer:Renderer, private _elementRef:ElementRef) {
+  constructor() {
   }
-
-  onTouched:() => any = () => {
-  };
-
-  controlValueAccessorChangeFn:(value:any) => void = (value)=> {
-  };
-
-  writeValue(value:any) {
-    this.checked = !!value;
-  }
-
-  registerOnChange(fn:(value:any) => void) {
-    this.controlValueAccessorChangeFn = fn;
-  }
-
-  registerOnTouched(fn:any) {
-    this.onTouched = fn;
-  }
-
 
 }
