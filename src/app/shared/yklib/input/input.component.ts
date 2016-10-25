ea01eb6fce17d5cce7ceea698206e6eaaf5615e4
杨恺 <njustyangkai@126.com>
2016-10-25 23:17:11 +0800

    css

--- src/app/shared/yklib/input/input.component.ts
+++ src/app/shared/yklib/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'yk-input',
@@ -7,5 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class InputComponent {
+  @Input() label:string;
+  @Input() labelWidth:string;
   showHint:boolean = false;
 }
