10a229d6609f0ddcbb178932d2744e0d5939078a
10195418 <yang.kai5@zte.com.cn>
2016-10-26 19:57:36 +0800

    pp

--- src/app/shared/yklib/input/input.component.ts
+++ src/app/shared/yklib/input/input.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+//noinspection TypeScriptCheckImport
+import { UUID } from 'angular2-uuid';
 
 @Component({
   selector: 'yk-input',
@@ -8,5 +10,11 @@ import { Component, Input } from '@angular/core';
 
 export class InputComponent {
   @Input() label:string;
+  inputId:string;
+
+  constructor() {
+    this.inputId = 'yk-input' + UUID.UUID();
+  }
+
   showHint:boolean = false;
 }
