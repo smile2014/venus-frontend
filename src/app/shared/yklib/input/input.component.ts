2d2227a58ea569ed783786dbaf06e8406d14f84d
10195418 <yang.kai5@zte.com.cn>
2016-10-26 11:14:10 +0800

    in

--- src/app/shared/yklib/input/input.component.ts
+++ src/app/shared/yklib/input/input.component.ts
@@ -9,5 +9,5 @@ import { Component, Input } from '@angular/core';
 export class InputComponent {
   @Input() label:string;
   @Input() labelWidth:string;
-  showHint:boolean = false;
+  showHint:boolean = true;
 }
