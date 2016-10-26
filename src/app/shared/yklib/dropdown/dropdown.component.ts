ccf0f9bf83a06c29069853e5b6010aa3bef66a7d
10195418 <yang.kai5@zte.com.cn>
2016-10-26 17:54:29 +0800

    dddd

--- src/app/shared/yklib/dropdown/dropdown.component.ts
+++ src/app/shared/yklib/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'yk-dropdown',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
 })
 
 export class DropdownComponent {
+  @Input() up:boolean;
+  @Input() text:string;
 
+  constructor() {
+    this.up = false;
+    this.text = '';
+  }
 }
