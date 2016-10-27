739914d497806cc176543c42ad56cafc892f4816
10195418 <yang.kai5@zte.com.cn>
2016-10-27 15:36:36 +0800

    df

--- src/app/demo/form/form.component.ts
+++ src/app/demo/form/form.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BaseModel } from '../../shared/yklib/form/base.model';
+import { SelectModel } from '../../shared/yklib/form/select.model';
+import { TextModel } from '../../shared/yklib/form/text.model';
 
 @Component({
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 
-export class FormComponent {
+export class FormComponent implements OnInit {
+  models:BaseModel<any>[];
 
+  constructor() {
+    this.models = [];
+  }
+
+  ngOnInit() {
+    this.models = this.getModels();
+  }
+
+  getModels() {
+    let models:BaseModel<any>[] = [
+      new SelectModel({
+        key: 'brave',
+        label: 'Bravery Rating',
+        options: [
+          {key: 'solid', value: 'Solid'},
+          {key: 'great', value: 'Great'},
+          {key: 'good', value: 'Good'},
+          {key: 'unproven', value: 'Unproven'}
+        ],
+        order: 3
+      }),
+      new TextModel({
+        key: 'firstName',
+        label: 'First name',
+        value: 'Bombasto',
+        order: 1
+      }),
+      new TextModel({
+        key: 'emailAddress',
+        label: 'Email',
+        type: 'email',
+        order: 2
+      })
+    ];
+    return models.sort((a, b) => a.order - b.order);
+  }
 }
--- src/app/shared/yklib/form/base.model.ts
+++ src/app/shared/yklib/form/base.model.ts
@@ -2,7 +2,7 @@ export class BaseModel<T> {
   value:T;
   key:string;
   label:string;
-  validators:[T];
+  validators:any[];
   order:number;
   controlType:string;
 
@@ -10,7 +10,7 @@ export class BaseModel<T> {
     value?:T,
     key?:string,
     label?:string,
-    validators?:{},
+    validators?:any[],
     order?:number,
     controlType?:string
   } = {}) {
